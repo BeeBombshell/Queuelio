05865ef024531453fc04e0d99403db184d21f6e3
Gaurav Dubey <gaurav.dubey@leverageedu.com>
2023-02-05 03:59:19 +0530

    api validation upgrades in joi

--- api/index.js
+++ api/index.js
@@ -13,31 +13,31 @@ let validator = require("express-joi-validation")({
 const ResHelper = require('../utils/response')
 global._validate = validator;
 app.use(express.json());
-require("./routes")(app, validator);
-app.use((err, req, res, next) => {
-  if (err.error.isJoi) {
-    let errDetail = [];
-    // we had a joi error, let's return a custom 400 json response
-    if (err.error.details) {
-      err.error.details.map(function (item) {
-        var temp = {};
-        temp[item.context.key] = item.message;
-        errDetail.push(temp);
-      });
-    }
+// require("./routes")(app, validator);
+// app.use((err, req, res, next) => {
+//   if (err.error.isJoi) {
+//     let errDetail = [];
+//     // we had a joi error, let's return a custom 400 json response
+//     if (err.error.details) {
+//       err.error.details.map(function (item) {
+//         var temp = {};
+//         temp[item.context.key] = item.message;
+//         errDetail.push(temp);
+//       });
+//     }
 
-    ResHelper.response(
-      res,
-      false,
-      "Submitted Information is not valid.",
-      400,
-      errDetail,
-      ""
-    );
-  } else {
-    ResHelper.response(res, false, "Error Occured.", 500, "", "");
-  }
-});
+//     ResHelper.response(
+//       res,
+//       false,
+//       "Submitted Information is not valid.",
+//       400,
+//       errDetail,
+//       ""
+//     );
+//   } else {
+//     ResHelper.response(res, false, "Error Occured.", 500, "", "");
+//   }
+// });
 app.use("/", router);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- api/models/index.js
+++ api/models/index.js
@@ -1,12 +1,12 @@
-const joi = require("joi");
-exports.newUser = {
+const joi = require("@hapi/joi");
+exports.newUser = joi.object({
   fname: joi.string().required(),
   lname: joi.string().optional().allow(null, ""),
   email: joi.string().required(),
   mobile: joi.string().optional().allow(null, ""),
   password: joi.string().required(),
-};
-exports.login = {
+});
+exports.login = joi.object({
   email: joi.string().required(),
   password: joi.string().required(),
-};
+});
